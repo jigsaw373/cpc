4f6bc317b63a1e77ccc906a0cfac5c831f7b9820
Mohammad <rezaparvizi1994@gmail.com>
2023-02-17 13:13:05 +0330

    refactor: split methods

--- cpc_test.go
+++ cpc_test.go
@@ -12,64 +12,82 @@ import (
 )
 
 func TestCopyBlockwise(t *testing.T) {
+	td := t.TempDir()
+
 	sizes := []int64{0, 1}
 	for n := 1; n < runtime.NumCPU(); n++ {
 		for delta := -1; delta <= 1; delta++ {
 			sizes = append(sizes, int64(n*(4<<10)+delta))
 		}
 	}
-	td := t.TempDir()
+
 	for _, size := range sizes {
-		t.Run(fmt.Sprintf("size%d", size), func(t *testing.T) {
-			ss := fmt.Sprint(size)
-			src := filepath.Join(td, "input-"+ss)
-			dst := filepath.Join(td, "output-"+ss)
-			want := randBytes(int(size))
-			if err := ioutil.WriteFile(src, want, 0644); err != nil {
-				t.Fatal(err)
-			}
-			// 3 runs: initial copy, no-op copy, single byte dirtied copy
-			for run := 1; run <= 3; run++ {
-				t.Run(fmt.Sprintf("run%d", run), func(t *testing.T) {
-					// On the 3rd run, dirty one random byte of the dst file.
-					if run == 3 {
-						if size == 0 {
-							t.Skip("n/a")
-						}
-						dirtyCopy := append([]byte(nil), want...)
-						dirtyCopy[mathrand.Intn(int(size))] = byte(mathrand.Intn(256))
-						if err := ioutil.WriteFile(dst, dirtyCopy, 0644); err != nil {
-							t.Fatal(err)
-						}
-					}
-					st, err := cpBlockwise(loggerDiscard, src, dst)
-					if err != nil {
-						t.Fatalf("cpblockwise: %v", err)
-					}
-					if run == 1 && st.PagesUnmodified != 0 {
-						t.Errorf("initial unmodified pages = %v; want 0", st.PagesUnmodified)
-					}
-					if run == 2 && st.PagesWritten > 0 {
-						t.Errorf("second run written pages = %v; want 0", st.PagesWritten)
-					}
-					if run == 3 {
-						if st.PagesWritten != 1 {
-							t.Errorf("PagesWritten = %v; want 1", st.PagesWritten)
-						}
-						if size > 4<<10 && st.PagesUnmodified == 0 {
-							t.Errorf("PagesUnmodified = %v; want >0", st.PagesUnmodified)
-						}
-					}
-					got, err := ioutil.ReadFile(dst)
-					if err != nil {
-						t.Fatal(err)
-					}
-					if !bytes.Equal(got, want) {
-						t.Fatalf("bytes didn't equal; dst len = %v; want len %v", len(got), size)
-					}
-				})
-			}
-		})
+		for run := 1; run <= 3; run++ {
+			name := fmt.Sprintf("size%d-run%d", size, run)
+			t.Run(name, func(t *testing.T) {
+				testCopyBlockwise(t, td, size, run)
+			})
+		}
+	}
+}
+
+func testCopyBlockwise(t *testing.T, td string, size int64, run int) {
+	ss := fmt.Sprint(size)
+	src := filepath.Join(td, "input-"+ss)
+	dst := filepath.Join(td, "output-"+ss)
+
+	want := randBytes(int(size))
+	if err := ioutil.WriteFile(src, want, 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	if run == 3 {
+		testDirtyCopy(t, size, want, dst)
+	}
+
+	st, err := cpBlockwise(loggerDiscard, src, dst)
+	if err != nil {
+		t.Fatalf("cpblockwise: %v", err)
+	}
+
+	testCopyResults(t, size, run, st)
+
+	got, err := ioutil.ReadFile(dst)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(got, want) {
+		t.Fatalf("bytes didn't equal; dst len = %v; want len %v", len(got), size)
+	}
+}
+
+func testDirtyCopy(t *testing.T, size int64, want []byte, dst string) {
+	if size == 0 {
+		t.Skip("n/a")
+	}
+
+	dirtyCopy := append([]byte(nil), want...)
+	dirtyCopy[mathrand.Intn(int(size))] = byte(mathrand.Intn(256))
+	if err := ioutil.WriteFile(dst, dirtyCopy, 0644); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func testCopyResults(t *testing.T, size int64, run int, st *stats) {
+	if run == 1 && st.PagesUnmodified != 0 {
+		t.Errorf("initial unmodified pages = %v; want 0", st.PagesUnmodified)
+	}
+	if run == 2 && st.PagesWritten > 0 {
+		t.Errorf("second run written pages = %v; want 0", st.PagesWritten)
+	}
+	if run == 3 {
+		if st.PagesWritten != 1 {
+			t.Errorf("PagesWritten = %v; want 1", st.PagesWritten)
+		}
+		if size > 4<<10 && st.PagesUnmodified == 0 {
+			t.Errorf("PagesUnmodified = %v; want >0", st.PagesUnmodified)
+		}
 	}
 }
 
